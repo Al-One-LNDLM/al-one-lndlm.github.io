6cfdbf1e376f0f27e44871154844ea43f5639513
Al-One-LNDLM <alejandrohdezantona@gmail.com>
2025-08-20 11:29:01 +0200

    feat: refine works popup interactions

--- config.js
+++ config.js
@@ -26,19 +26,51 @@ export const zones = [
     img: 'assets/Edificio4.png',
     listLabel: 'assets/text2.png',
     position: { top: '3vh', right: '6vw' },
-    // Duplica el bloque <a class="video-card">…</a> para añadir más videos
     popup: {
       title: 'Trabajos',
       content: `
         <div class="trabajos-gallery">
-          <a class="video-card" href="https://www.youtube.com/watch?v=9gNmEuoKf7c&list=RD9gNmEuoKf7c&start_radio=1" target="_blank">
-            <div class="thumb" style="background-image:url('assets/mini1.jpg')"></div>
-            <p>El Niño y Su Fe (ALbumetraje Oficial)</p>
-          </a>
-          <a class="video-card" href="https://www.youtube.com/watch?v=oHg5SJYRHA0" target="_blank">
-            <div class="thumb" style="background-image:url('assets/mini1.jpg')"></div>
-            <p>Video ejemplo 2</p>
-          </a>
+          <!-- Álbum 1 -->
+          <div class="work-album">
+            <a class="thumb-link" href="https://www.youtube.com/playlist?list=ALBUM1" target="_blank">
+              <img class="thumb" src="assets/mini1.jpg" alt="Álbum 1">
+            </a>
+            <div class="info">
+              <a class="title-link" href="https://www.youtube.com/playlist?list=ALBUM1" target="_blank"><h3>Álbum 1</h3></a>
+              <p>Descripción breve del álbum.</p>
+            </div>
+          </div>
+
+          <!-- Canciones del Álbum 1 -->
+          <div class="work-song">
+            <a class="thumb-link" href="https://youtu.be/9gNmEuoKf7c" target="_blank"><img class="thumb" src="assets/mini1.jpg" alt="Canción 1"></a>
+            <a class="title-link" href="https://youtu.be/9gNmEuoKf7c" target="_blank">Canción 1</a>
+          </div>
+          <div class="work-song">
+            <a class="thumb-link" href="https://youtu.be/oHg5SJYRHA0" target="_blank"><img class="thumb" src="assets/mini1.jpg" alt="Canción 2"></a>
+            <a class="title-link" href="https://youtu.be/oHg5SJYRHA0" target="_blank">Canción 2</a>
+          </div>
+
+          <!-- Álbum 2 -->
+          <div class="work-album">
+            <a class="thumb-link" href="https://www.youtube.com/playlist?list=ALBUM2" target="_blank">
+              <img class="thumb" src="assets/mini1.jpg" alt="Álbum 2">
+            </a>
+            <div class="info">
+              <a class="title-link" href="https://www.youtube.com/playlist?list=ALBUM2" target="_blank"><h3>Álbum 2</h3></a>
+              <p>Otra descripción del álbum.</p>
+            </div>
+          </div>
+
+          <!-- Canciones del Álbum 2 -->
+          <div class="work-song">
+            <a class="thumb-link" href="https://youtu.be/oHg5SJYRHA0" target="_blank"><img class="thumb" src="assets/mini1.jpg" alt="Canción A"></a>
+            <a class="title-link" href="https://youtu.be/oHg5SJYRHA0" target="_blank">Canción A</a>
+          </div>
+          <div class="work-song">
+            <a class="thumb-link" href="https://youtu.be/oHg5SJYRHA0" target="_blank"><img class="thumb" src="assets/mini1.jpg" alt="Canción B"></a>
+            <a class="title-link" href="https://youtu.be/oHg5SJYRHA0" target="_blank">Canción B</a>
+          </div>
         </div>
       `
     }
