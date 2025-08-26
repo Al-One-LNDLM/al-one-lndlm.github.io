28bdf0a608ca95e17f120ebea05d3ca820441ced
Al-One-LNDLM <alejandrohdezantona@gmail.com>
2025-08-26 14:13:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ export const zones = [
     id: 'trabajos',
     img: 'assets/Edificio4.png',
     listLabel: 'assets/text2.png',
-    position: { top: '3vh', right: '4.8vw' },
+    position: { top: '3.5vh', right: '4.8vw' },
     popup: {
       title: 'Trabajos',
       content: `
